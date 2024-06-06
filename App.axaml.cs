9144550d38aa1a3553acdeb21243de323d956b92
jourim66 <jourim66@gmail.com>
2024-06-07 01:25:28 +0500

    Second

--- App.axaml.cs
+++ App.axaml.cs
@@ -12,64 +12,17 @@ using avDDBug.Views;
 namespace avDDBug;
 
 public partial class App : Application {
-    public enum CatalogTheme {
-        Fluent,
-        Simple
-    }
-    private readonly Styles _themeStylesContainer = new();
-    private FluentTheme? _fluentTheme;
-    private SimpleTheme? _simpleTheme;
     public override void Initialize() {
-        Styles.Add( _themeStylesContainer );
-
         AvaloniaXamlLoader.Load( this );
-
-        _fluentTheme = new FluentTheme();
-        _simpleTheme = new SimpleTheme();
-
-        SetCatalogThemes( CatalogTheme.Fluent );
     }
-
     public override void OnFrameworkInitializationCompleted() {
         if( ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop ) {
             BindingPlugins.DataValidators.RemoveAt( 0 );
-            desktop.MainWindow = new MainWindow();
+            desktop.MainWindow = new MainWindow {
+                DataContext = new MainWindowViewModel(),
+            };
         }
 
         base.OnFrameworkInitializationCompleted();
-    }
-
-    private CatalogTheme _prevTheme;
-    public static CatalogTheme CurrentTheme => ( (App)Current! )._prevTheme;
-
-    public static void SetCatalogThemes( CatalogTheme theme ) {
-        var app = (App)Current!;
-        var prevTheme = app._prevTheme;
-        app._prevTheme = theme;
-        var shouldReopenWindow = prevTheme != theme;
-
-        if( app._themeStylesContainer.Count == 0 ) {
-            app._themeStylesContainer.Add( new Style() );
-        }
-
-        if( theme == CatalogTheme.Fluent ) {
-            app._themeStylesContainer[ 0 ] = app._fluentTheme!;
-        } else if( theme == CatalogTheme.Simple ) {
-            app._themeStylesContainer[ 0 ] = app._simpleTheme!;
-        }
-
-        if( shouldReopenWindow ) {
-            if( app.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopLifetime ) {
-                var oldWindow = desktopLifetime.MainWindow;
-                var newWindow = new MainWindow();
-
-                if ( oldWindow is not null ) 
-                    newWindow.Position = oldWindow.Position;
-
-                desktopLifetime.MainWindow = newWindow;
-                newWindow.Show();
-                oldWindow?.Close();
-            }
-        }
     }
 }
--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -4,46 +4,18 @@ using System.ComponentModel;
 
 namespace avDDBug.ViewModels;
 
-public record TableDisplayInfo {
-    public string Description { get; set; }
-    public string TypeName { get; set; }
-    public string Count { get; set; }
-    public TableDisplayInfo() {
-        Count = string.Empty;
-        TypeName = "<none>";
-        Description = string.Empty;
-    }
-    public TableDisplayInfo( string tp, string file, int cn ) {
-        TypeName = tp;
-        Description = file;
-        Count = cn.ToString();
-    }
-}
-
 public partial class MainWindowViewModel : ObservableValidator, INotifyPropertyChanged {
-    [ObservableProperty]IList<TableDisplayInfo> list = [];
     [ObservableProperty]int itemIndex;
-    [ObservableProperty]TableDisplayInfo? curItem;
-    string ThemeName {
-        get => $"Theme is \"{App.CurrentTheme}\"";
-    }
-    bool IsFluentTheme {
-        get => App.CurrentTheme == App.CatalogTheme.Fluent;
-        set => App.SetCatalogThemes( value ? App.CatalogTheme.Fluent : App.CatalogTheme.Simple );
+    
+    string SimpleStringData = "SimpleStringData";
+    string SimpleStringProp {
+        get => $"Theme is \"{SimpleStringData}\"";
     }
 
     public MainWindowViewModel() {
-        List = [
-            new TableDisplayInfo(),
-            new TableDisplayInfo( "tp1", "decs1", 10 ),
-            new TableDisplayInfo( "typep2", "decs2", 100 ),
-            new TableDisplayInfo( "type point3", "decs3", 1000 ),
-            new TableDisplayInfo( "some other long type tp4", "decs4", 10000 ),
-            ];
     }
 
     [RelayCommand]
     private void Change() {
-        App.SetCatalogThemes( App.CurrentTheme == App.CatalogTheme.Simple ? App.CatalogTheme.Fluent : App.CatalogTheme.Simple );
     }
 }
