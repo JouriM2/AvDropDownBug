d1c844902febe3815966a762966ca5e82ae7fba5
jourim66 <jourim66@gmail.com>
2024-06-07 01:57:40 +0500

    Second

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -1,21 +1,11 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.ComponentModel;
 
 namespace avDDBug.ViewModels;
 
 public partial class MainWindowViewModel : ObservableValidator, INotifyPropertyChanged {
-    [ObservableProperty]int itemIndex;
-    
-    string SimpleStringData = "SimpleStringData";
-    string SimpleStringProp {
-        get => $"Theme is \"{SimpleStringData}\"";
-    }
-
     public MainWindowViewModel() {
-    }
-
-    [RelayCommand]
-    private void Change() {
     }
 }
